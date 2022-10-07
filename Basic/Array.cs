e58a1e7ff760cfc5fbee8083dec9e7e4941022be
black <black@LAPTOP-QT5SFJG4>
2022-10-07 17:48:35 +0300

    Add project files.

---
+++ Basic/Array.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Basic
+{
+    public class Array
+    {
+        public int[] array;
+        public int smallest;
+        public int[] RandomArray(int n,int max, int min)
+        {
+            try
+            {
+                Random random = new Random();
+                array = new int[n];
+
+                int j = 0;
+                while (j < n)
+                {
+                    array[j] = random.Next(min,max);
+                    j++;
+                }
+                return array;
+            }
+            catch (Exception _ex)  //dizi oluşturulrken her hangi bir hata oluşursa ayrıca
+            {
+                Console.WriteLine("Dizi Oluşturulurken Hata Oluştu." + _ex.Message);
+                return null;
+            }
+        }
+
+        public int[] ShowArray(int length)
+        {
+            int k = 0;
+            while (k < length)
+            {
+                Console.Write( array[k] + "   ");
+                k++;
+            }
+            return array;
+        }
+
+        public int DetermineSmallest()
+        {
+                if (array != null)
+                {
+                    for (int l = 0; l < array.Length; l++)
+                    {
+                        if (array[l] > 0) //eğer smallesta kafadan direk 10 verirsek mesela diğer döngüde smallest>0 old için 
+                        {
+                            smallest = array[l];
+                            break;
+                        }
+                    }
+                    for (int i = 0; i < array.Length; i++)  //smallest değerini dizi üzerinde gezdiriyoruz smallest dan daha küçük bir sayıyla karşılaştığımızda yeni smallest o oluyor
+                    {
+                        if (array[i] > 0)
+                        {
+                            if (smallest > array[i])
+                            {
+                                smallest = array[i];
+                            }
+                        }
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("None of The Array's Index Meet Your Requirement ");   //tedbir amaçlı dizideki hiçbir şey olmazsa nolcak bunu yazdırcak try/catch de yapmaya çalış
+                }
+                Console.WriteLine("\nThe smallest number in the array is:{0}", smallest); // \n bu yazıyı yazdırmadan önce alt satıra geçsin WriteLine da smallest bu yazının yanına gelsin       
+                return smallest;
+        }
+    }
+}
---
+++ Basic/Program.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Basic
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Press 'N'");
+            char c = Convert.ToChar(Console.ReadLine());
+            //while (true)
+            //{
+            if (c == Convert.ToChar(ConsoleKey.N))
+            {
+                while (c == Convert.ToChar(ConsoleKey.N))
+                {
+                    int CountDown = 0;
+                    int login = 3;
+                    int min;
+                    int max;
+                    int length;
+                    Array myArray = new Array();
+
+                    while (true)
+                    {
+                        try
+                        {
+                            Console.Write("Please enter the min:");
+                            min = int.Parse(Console.ReadLine());
+                            Console.Write("Please enter the max:");
+                            max = int.Parse(Console.ReadLine());
+                            if(min>=max)
+                            {
+                                Console.WriteLine("Min values couldn't greater than the max values...");
+                                Thread.Sleep(800);
+                                continue;
+                            }
+                            Console.Write("Enter the array length:");  //bunu döngü içine alıp min max kontrolü sağlayabilir miyiz
+                            length = int.Parse(Console.ReadLine());
+                            break;
+                        }
+                        catch (Exception)
+                        {
+                            if (CountDown < 2)
+                            {
+                                CountDown++;
+                                login--;
+                                Console.WriteLine("This Value İs İnvalid. You have {0} left", login);
+                                continue;
+                            }
+                            else
+                            {
+                                Console.WriteLine("Console is closing...");
+                                Thread.Sleep(2000);
+                                Environment.Exit(0);
+                            }
+                        }
+                    }
+                        myArray.RandomArray(length, max, min);
+                        myArray.ShowArray(length);
+
+                        Stopwatch _sw2 = new Stopwatch();
+                        _sw2.Start();
+                        myArray.DetermineSmallest();
+                        _sw2.Stop();
+                        Console.WriteLine("0'dan büyük en küçük Sayının Bulunması " + _sw2.ElapsedMilliseconds + " ms sürdü.");
+                        Console.WriteLine("To Exit Press 'Q' , To Continue Press 'N' ");
+                        c = Convert.ToChar(Console.ReadLine());
+
+                        if (c == Convert.ToChar(ConsoleKey.N))
+                        {
+                            continue;
+                        }
+                        else if (c == Convert.ToChar(ConsoleKey.Q))
+                        {
+                            Console.WriteLine("You Pressed 'Q'. Program is Closing...");
+                            Thread.Sleep(2500);
+                            Environment.Exit(0);
+                        }
+                }
+            }
+            else
+            {
+                Console.WriteLine("Program Kapatılıyor N'ye basmadınız...");
+                Thread.Sleep(1500);
+            }
+        }
+    }
+}
